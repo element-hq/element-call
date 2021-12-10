c5728b2f66748a70b1ba1bb0bf1159f13da96cf8
Robert Long <robert@robertlong.me>
2021-12-10 13:44:06 -0800

    Add room id routing

--- src/InviteModal.jsx
+++ src/InviteModal.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Modal, ModalContent } from "./Modal";
 import { CopyButton } from "./button";
+import { getRoomUrl } from "./ConferenceCallManagerHooks";
 
-export function InviteModal({ roomUrl, ...rest }) {
+export function InviteModal({ roomId, ...rest }) {
   return (
     <Modal title="Invite People" isDismissable {...rest}>
       <ModalContent>
         <p>Copy and share this meeting link</p>
-        <CopyButton value={roomUrl} />
+        <CopyButton value={getRoomUrl(roomId)} />
       </ModalContent>
     </Modal>
   );
