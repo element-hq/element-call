2c36f6a9df0a10673b2a786238f01f260bf919dc
Robert Long <robert@robertlong.me>
2022-02-03 14:18:34 -0800

    Fix null recaptcha target

--- src/auth/useRecaptcha.js
+++ src/auth/useRecaptcha.js
@@ -64,7 +64,7 @@ export function useRecaptcha(sitekey) {
     return new Promise((resolve, reject) => {
       const observer = new MutationObserver((mutationsList) => {
         for (const item of mutationsList) {
-          if (item.target.style?.visibility !== "visible") {
+          if (item.target?.style?.visibility !== "visible") {
             reject(new Error("Recaptcha dismissed"));
             observer.disconnect();
             return;
