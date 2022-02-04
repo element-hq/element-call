1d8cd8c3c81cbc7a3e09836497bbb1f0aa799a4f
Robert Long <robert@robertlong.me>
2022-02-03 16:56:13 -0800

    Add useLocationNavigation to fix navigation during browser media prompts

--- src/room/LobbyView.jsx
+++ src/room/LobbyView.jsx
@@ -14,6 +14,7 @@ import { getAvatarUrl } from "../matrix-utils";
 import { useProfile } from "../profile/useProfile";
 import useMeasure from "react-use-measure";
 import { ResizeObserver } from "@juggle/resize-observer";
+import { useLocationNavigation } from "../useLocationNavigation";
 
 export function LobbyView({
   client,
@@ -39,6 +40,8 @@ export function LobbyView({
   useEffect(() => {
     onInitLocalCallFeed();
   }, [onInitLocalCallFeed]);
+
+  useLocationNavigation(state === GroupCallState.InitializingLocalCallFeed);
 
   return (
     <div className={styles.room}>
