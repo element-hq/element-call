b231424f9699b6cba92c0faad35d9b597f8648c3
Robert Long <robert@robertlong.me>
2022-05-10 13:04:04 -0700

    Reference vite/svgr types

--- src/@types/modules.d.ts
+++ src/@types/modules.d.ts
@@ -1,11 +1,2 @@
-declare module "*.module.css" {
-  const classes: {
-    [key: string]: string;
-  };
-  export default classes;
-}
-declare module "*.svg" {
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGAttributes<SVGElement>
-  >;
-}
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
