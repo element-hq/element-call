e1745d585e0fcbc6b0f34ac57ab36af217b708bd
Robin Townsend <robin@robin.town>
2022-06-01 10:41:49 -0400

    Bump the animation size up a little bit more

--- src/room/PTTButton.tsx
+++ src/room/PTTButton.tsx
@@ -135,7 +135,7 @@ export const PTTButton: React.FC<Props> = ({
   }, [onWindowMouseUp, onWindowTouchEnd, onButtonTouchStart, buttonRef]);
 
   const { shadow } = useSpring({
-    shadow: (Math.max(activeSpeakerVolume, -70) + 70) * 0.5,
+    shadow: (Math.max(activeSpeakerVolume, -70) + 70) * 0.6,
     config: {
       clamp: true,
       tension: 300,
