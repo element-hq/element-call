f23195657b6335cc7829b8160ee9a162d2ae9c70
hugohutri <55588133+hugohutri@users.noreply.github.com>
2022-07-23 16:52:37 +0300

    wip: voice activity detection

--- src/video-grid/useCallFeed.ts
+++ src/video-grid/useCallFeed.ts
@@ -77,7 +77,10 @@ export function useCallFeed(callFeed: CallFeed): CallFeedState {
       callFeed.on(CallFeedEvent.MuteStateChanged, onMuteStateChanged);
       callFeed.on(CallFeedEvent.LocalVolumeChanged, onLocalVolumeChanged);
       callFeed.on(CallFeedEvent.NewStream, onUpdateCallFeed);
-      callFeed.on(CallFeedEvent.VoiceActivityTresholdChanged, onVoiceActivityTresholdChanged);
+      callFeed.on(
+        CallFeedEvent.VoiceActivityTresholdChanged,
+        onVoiceActivityTresholdChanged
+      );
     }
 
     onUpdateCallFeed();
