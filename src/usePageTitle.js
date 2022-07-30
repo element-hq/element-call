5841c4f38d22456d50b97d7ea39b0002371b6536
Šimon Brandner <simon.bra.ag@gmail.com>
2022-07-30 09:59:51 +0200

    `usePageTitle`
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/usePageTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export function usePageTitle(title) {
-  useEffect(() => {
-    const productName =
-      import.meta.env.VITE_PRODUCT_NAME || "Matrix Video Chat";
-    document.title = title ? `${productName} | ${title}` : productName;
-  }, [title]);
-}
