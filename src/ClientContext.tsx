6b5c76cfc7af7eebd56e4514cbaf2fe77f83ca0a
Timo K <timok@element.io>
2022-09-13 16:03:39 +0200

    fix logout

--- src/ClientContext.tsx
+++ src/ClientContext.tsx
@@ -256,10 +256,19 @@ export const ClientProvider: FC<Props> = ({ children }) => {
     [client]
   );
 
-  const logout = useCallback(() => {
+  const logout = useCallback(async () => {
+    await client.logout(undefined, true);
     clearSession();
+    setState({
+      client: undefined,
+      loading: false,
+      isAuthenticated: false,
+      isPasswordlessUser: true,
+      userName: "",
+      error: undefined,
+    });
     history.push("/");
-  }, [history]);
+  }, [history, client]);
 
   useEffect(() => {
     if (client) {
