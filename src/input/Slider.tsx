8bbd9a19e1f1f5941f63d560841b1780bcc8de27
Dashie <fabio.lenherr@gmail.com>
2022-10-06 08:30:53 +0200

    Update src/input/Slider.tsx
    Co-authored-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/input/Slider.tsx
+++ src/input/Slider.tsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Hugo Hutri <hugo.hutri98@gmail.com>
+Copyright 2022 Hugo Hutri <hugo.hutri98@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
