0e478f4c20c8b88350edfeaa35d166f5c94068dd
Robin Townsend <robin@robin.town>
2022-11-09 10:53:33 -0500

    Add config documentation, and better types

--- src/config/ConfigOptions.ts
+++ src/config/ConfigOptions.ts
@@ -1,18 +1,37 @@
-export interface IConfigOptions {
+export interface ConfigOptions {
+  /**
+   * The Posthog endpoint to which analytics data will be sent.
+   */
   posthog?: {
     api_key: string;
     api_host: string;
   };
+  /**
+   * The Sentry endpoint to which crash data will be sent.
+   */
   sentry?: {
     DSN: string;
     environment: string;
   };
+  /**
+   * The rageshake server to which feedback and debug logs will be sent.
+   */
   rageshake?: {
     submit_url: string;
   };
 }
 
-export const DEFAULT_CONFIG: IConfigOptions = {
+export interface ResolvedConfigOptions extends ConfigOptions {
+  sentry: {
+    DSN: string;
+    environment: string;
+  };
+  rageshake: {
+    submit_url: string;
+  };
+}
+
+export const DEFAULT_CONFIG: ResolvedConfigOptions = {
   sentry: { DSN: "", environment: "production" },
   rageshake: {
     submit_url: "https://element.io/bugreports/submit",
