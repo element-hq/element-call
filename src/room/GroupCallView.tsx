04f0179c5657d134d3c0194b0f4dfc2ae3edce8f
Abhi Jain <abhijain3002@gmail.com>
2023-02-08 14:26:56 +0530

    changes Loading room... to Loading...

--- src/room/GroupCallView.tsx
+++ src/room/GroupCallView.tsx
@@ -279,7 +279,7 @@ export function GroupCallView({
   } else if (isEmbedded) {
     return (
       <FullScreenView>
-        <h1>{t("Loading room…")}</h1>
+        <h1>{t("Loading...")}</h1>
       </FullScreenView>
     );
   } else {
