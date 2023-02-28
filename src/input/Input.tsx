e281c3f1504bb27d484f0eedac55e22e189e1753
Timo K <toger5@hotmail.de>
2023-02-28 19:02:38 +0100

    make label optional in InputFieldProps

--- src/input/Input.tsx
+++ src/input/Input.tsx
@@ -55,7 +55,7 @@ export function Field({ children, className }: FieldProps): JSX.Element {
 }
 
 interface InputFieldProps {
-  label: string;
+  label?: string;
   type: string;
   prefix?: string;
   suffix?: string;
