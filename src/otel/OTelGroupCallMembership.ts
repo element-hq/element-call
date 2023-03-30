3a7983d2de2e160f009d04f9764ed3e3b144e61f
David Baker <dave@matrix.org>
2023-03-30 14:07:49 +0100

    Add displayname on call spans

--- src/otel/OTelGroupCallMembership.ts
+++ src/otel/OTelGroupCallMembership.ts
@@ -170,6 +170,9 @@ export class OTelGroupCallMembership {
           // XXX: anonymity
           span.setAttribute("matrix.call.target.userId", userId);
           span.setAttribute("matrix.call.target.deviceId", deviceId);
+
+          const displayName = this.groupCall.room.getMember(userId)?.name;
+          span.setAttribute("matrix.call.target.displayName", displayName);
           this.callsByCallId.set(call.callId, {
             userId,
             deviceId,
