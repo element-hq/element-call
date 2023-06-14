20c9c092586983e206a37576a23346e9dc012aea
Enrico Schwendig <enricos@element.io>
2023-06-14 15:03:38 +0200

    Build EC room use method (#1108)
    * Build custom useRoom

--- src/room/InCallView.tsx
+++ src/room/InCallView.tsx
@@ -16,7 +16,6 @@ limitations under the License.
 
 import { ResizeObserver } from "@juggle/resize-observer";
 import {
-  useLiveKitRoom,
   useLocalParticipant,
   useParticipants,
   useToken,
@@ -79,12 +78,23 @@ import { InviteModal } from "./InviteModal";
 import { useRageshakeRequestModal } from "../settings/submit-rageshake";
 import { RageshakeRequestModal } from "./RageshakeRequestModal";
 import { VideoTile } from "../video-grid/VideoTile";
+import { useRoom } from "./useRoom";
 
 const canScreenshare = "getDisplayMedia" in (navigator.mediaDevices ?? {});
 // There is currently a bug in Safari our our code with cloning and sending MediaStreams
 // or with getUsermedia and getDisplaymedia being used within the same session.
 // For now we can disable screensharing in Safari.
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+const onConnectedCallback = (): void => {
+  console.log("connected to LiveKit room");
+};
+const onDisconnectedCallback = (): void => {
+  console.log("disconnected from LiveKit room");
+};
+const onErrorCallback = (err: Error): void => {
+  console.error("error connecting to LiveKit room", err);
+};
 
 interface LocalUserChoices {
   videoMuted: boolean;
@@ -150,22 +160,16 @@ export function InCallView({
     options
   );
 
-  // Uses a hook to connect to the LiveKit room (on unmount the room will be left) and publish local media tracks (default).
-  useLiveKitRoom({
+  useRoom({
     token,
     serverUrl: Config.get().livekit.server_url,
     room: livekitRoom,
     audio: !userChoices.audioMuted,
     video: !userChoices.videoMuted,
-    onConnected: () => {
-      console.log("connected to LiveKit room");
-    },
-    onDisconnected: () => {
-      console.log("disconnected from LiveKit room");
-    },
-    onError: (err) => {
-      console.error("error connecting to LiveKit room", err);
-    },
+    simulateParticipants: 10,
+    onConnected: onConnectedCallback,
+    onDisconnected: onDisconnectedCallback,
+    onError: onErrorCallback,
   });
 
   const screenSharingTracks = useTracks(
