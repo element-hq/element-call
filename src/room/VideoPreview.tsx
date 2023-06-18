b8b35cc7bb695e1102ce84e992652389ef5e6c17
Robin Townsend <robin@robin.town>
2023-06-18 14:26:50 -0400

    Fix a likely bug with audio inputs and outputs getting mixed up?

--- src/room/VideoPreview.tsx
+++ src/room/VideoPreview.tsx
@@ -79,7 +79,7 @@ export function VideoPreview({ matrixInfo, onUserChoicesChanged }: Props) {
   );
   const audio = usePreviewDevice(
     audioEnabled,
-    audioId != "" ? audioId : defaultDevices.audiooutput,
+    audioId != "" ? audioId : defaultDevices.audioinput,
     "audioinput"
   );
 
