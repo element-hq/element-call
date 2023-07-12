8946af8f4e5e86b94198f39a1fe7f4270dace997
Šimon Brandner <simon.bra.ag@gmail.com>
2023-07-12 17:50:07 +0200

    Hack e2ee in
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/livekit/options.ts
+++ src/livekit/options.ts
@@ -6,7 +6,9 @@ import {
   TrackPublishDefaults,
   VideoPreset,
   VideoPresets,
+  ExternalE2EEKeyProvider,
 } from "livekit-client";
+import E2EEWorker from "livekit-client/e2ee-worker?worker";
 
 const defaultLiveKitPublishOptions: TrackPublishDefaults = {
   audioPreset: AudioPresets.music,
@@ -22,7 +24,16 @@ const defaultLiveKitPublishOptions: TrackPublishDefaults = {
   backupCodec: { codec: "vp8", encoding: VideoPresets.h720.encoding },
 } as const;
 
+const e2eeWorker = new E2EEWorker();
+const e2eeKeyProvider = new ExternalE2EEKeyProvider();
+e2eeKeyProvider.setKey("not secret password");
+
 export const defaultLiveKitOptions: RoomOptions = {
+  e2ee: {
+    keyProvider: e2eeKeyProvider,
+    worker: e2eeWorker,
+  },
+
   // automatically manage subscribed video quality
   adaptiveStream: true,
 
