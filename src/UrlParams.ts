28b46be04356e34b46c1d4c2a1b38d9a6d039e4f
Timo <16718859+toger5@users.noreply.github.com>
2023-10-25 13:49:18 +0200

    Add skipLobby to params (#1730)
    ---------
    
    Signed-off-by: Timo K <toger5@hotmail.de>

--- src/UrlParams.ts
+++ src/UrlParams.ts
@@ -111,6 +111,12 @@ interface UrlParams {
    * E2EE password
    */
   password: string | null;
+  /**
+   * Setting this flag skips the lobby and brings you in the call directly.
+   * In the widget this can be combined with preload to pass the device settings
+   * with the join widget action.
+   */
+  skipLobby: boolean;
 }
 
 // This is here as a stopgap, but what would be far nicer is a function that
@@ -206,6 +212,7 @@ export const getUrlParams = (
     fontScale: Number.isNaN(fontScale) ? null : fontScale,
     analyticsID: parser.getParam("analyticsID"),
     allowIceFallback: parser.getFlagParam("allowIceFallback"),
+    skipLobby: parser.getFlagParam("skipLobby"),
   };
 };
 
