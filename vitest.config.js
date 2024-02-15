f32f3e601c8d465720136adab93d5201c96bdc30
Angel Mendez Cano <amendez1988@gmail.com>
2024-02-15 17:41:49 -0600

    chore: remove alias on vitest config file
    - alias is no needed

--- vitest.config.js
+++ vitest.config.js
@@ -7,12 +7,6 @@ export default defineConfig(configEnv => mergeConfig(
   test: {
     globals: true,
     environment: 'jsdom',
-    alias: {
-      "\\.css$": "identity-obj-proxy",
-      "\\.svg\\?react$": "<rootDir>/test/mocks/svgr.ts",
-      "^\\./IndexedDBWorker\\?worker$": "<rootDir>/test/mocks/workerMock.ts",
-      "^\\./olm$": "<rootDir>/test/mocks/olmMock.ts"
-    },
     css: {
       modules: {
         classNameStrategy: 'non-scoped'
