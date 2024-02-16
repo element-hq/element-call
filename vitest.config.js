458f7cdfb1b35c39c75491132887e39f6265b206
Robin <robin@robin.town>
2024-02-15 22:34:59 -0500

    Format code

--- vitest.config.js
+++ vitest.config.js
@@ -1,25 +1,24 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
-export default defineConfig(configEnv => mergeConfig(
-  viteConfig(configEnv),
-  defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    css: {
-      modules: {
-        classNameStrategy: 'non-scoped'
-      }
-    },
-    include: [
-      "test/**/*-test.[jt]s?(x)"
-    ],
-    coverage: {
-      reporter: ['text', 'html'],
-      exclude: [
-        'node_modules/'
-      ]
-    }
-  }
-})))
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        css: {
+          modules: {
+            classNameStrategy: "non-scoped",
+          },
+        },
+        include: ["test/**/*-test.[jt]s?(x)"],
+        coverage: {
+          reporter: ["text", "html"],
+          exclude: ["node_modules/"],
+        },
+      },
+    }),
+  ),
+);
