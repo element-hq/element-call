f517099f271eda386d822707db208dd707836af1
fkwp <github-fkwp@w4ve.de>
2024-09-06 10:35:10 +0200

    The New Vector Ltd. -> New Vector Ltd

--- src/analytics/PosthogAnalytics.ts
+++ src/analytics/PosthogAnalytics.ts
@@ -1,6 +1,5 @@
 /*
-Copyright 2024 New Vector Ltd.
-Copyright 2022-2024 The New Vector Ltd
+Copyright 2022-2024 New Vector Ltd.
 
 SPDX-License-Identifier: AGPL-3.0-only
 Please see LICENSE in the repository root for full details.
