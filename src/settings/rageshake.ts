55ea373cd03cedcb9651055772bd084658343fe5
Hugh Nimmo-Smith <hughns@element.io>
2024-09-11 12:20:30 +0100

    Intercept matrix_sdk logging via console

--- src/settings/rageshake.ts
+++ src/settings/rageshake.ts
@@ -467,11 +467,35 @@ declare global {
  */
 export async function init(): Promise<void> {
   global.mx_rage_logger = new ConsoleLogger();
+
+  // configure loglevel based loggers:
   setLogExtension(logger, global.mx_rage_logger.log);
   // these are the child/prefixed loggers we want to capture from js-sdk
   // there doesn't seem to be an easy way to capture all children
   ["MatrixRTCSession", "MatrixRTCSessionManager"].forEach((loggerName) => {
     setLogExtension(logger.getChild(loggerName), global.mx_rage_logger.log);
+  });
+
+  // intercept console logging so that we can get matrix_sdk logs:
+  // this is nasty, but no logging hooks are provided
+  (
+    ["trace", "debug", "info", "warn", "error"] as (
+      | "trace"
+      | "debug"
+      | "info"
+      | "warn"
+      | "error"
+    )[]
+  ).forEach((level) => {
+    if (!window.console[level]) return;
+    const prefix = `${level.toUpperCase()} matrix_sdk`;
+    const originalMethod = window.console[level];
+    window.console[level] = (...args): void => {
+      originalMethod(...args);
+      if (typeof args[0] === "string" && args[0].startsWith(prefix)) {
+        global.mx_rage_logger.log(LogLevel[level], "matrix_sdk", ...args);
+      }
+    };
   });
 
   return tryInitStorage();
