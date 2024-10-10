1d1db2a4f85390c757829c6e777ca2ff15bd8b34
Hugh Nimmo-Smith <hughns@users.noreply.github.com>
2024-10-10 10:34:38 +0100

    Set LiveKit keyring size to 256 (#2660)

--- src/e2ee/matrixKeyProvider.ts
+++ src/e2ee/matrixKeyProvider.ts
@@ -16,7 +16,7 @@ export class MatrixKeyProvider extends BaseKeyProvider {
   private rtcSession?: MatrixRTCSession;
 
   public constructor() {
-    super({ ratchetWindowSize: 0 });
+    super({ ratchetWindowSize: 0, keyringSize: 256 });
   }
 
   public setRTCSession(rtcSession: MatrixRTCSession): void {
