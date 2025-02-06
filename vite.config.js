10fe734c2e45b27cc2d6705bd545b0e1c577464c
Hugh Nimmo-Smith <hughns@element.io>
2025-02-06 10:44:05 +0000

    Don't produce compressed assets at build time and instead do it as part of the nginx build
    The core vite build shouldn't be responsible for implementing optimisations that are specific to a particular distribution mechanism. e.g. static compressed files for nginx.
    
    This way the core build is "clean".
    
    Instead, the packager/distributor of the build should be responsible for it. e.g. the nginx docker distribution should do any optimisations that it chooses.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ Please see LICENSE in the repository root for full details.
 */
 
 import { defineConfig, loadEnv } from "vite";
-import { compression } from "vite-plugin-compression2";
 import svgrPlugin from "vite-plugin-svgr";
 import htmlTemplate from "vite-plugin-html-template";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
@@ -38,10 +37,6 @@ export default defineConfig(({ mode }) => {
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
       bundleName: "element-call",
       uploadToken: process.env.CODECOV_TOKEN,
-    }),
-
-    compression({
-      exclude: [/config.json/],
     }),
   ];
 
