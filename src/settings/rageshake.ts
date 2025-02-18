f35e48fba1bdaff337be77949313c5e0800fee4c
Hugh Nimmo-Smith <hughns@users.noreply.github.com>
2025-02-18 17:59:58 +0000

    Fix copyright header to say dual license not just AGPL (#3013)
    This probably should have been part of https://github.com/element-hq/element-call/pull/2984

--- src/settings/rageshake.ts
+++ src/settings/rageshake.ts
@@ -2,7 +2,7 @@
 Copyright 2018-2024 New Vector Ltd.
 Copyright 2017 OpenMarket Ltd
 
-SPDX-License-Identifier: AGPL-3.0-only
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE in the repository root for full details.
 */
 
