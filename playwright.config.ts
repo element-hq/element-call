decdc8e8fe342a2396e7e4d798ded2b8c33d60f3
Valere <bill.carson@valrsoft.com>
2025-03-14 14:50:47 +0100

    code review

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig, devices } from "@playwright/test";
+/*
+Copyright 2025 New Vector Ltd.
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE in the repository root for full details.
+*/
+
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
