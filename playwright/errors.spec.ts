55643649a2414cb2184feac88a9a718b9a52fa34
Valere <bill.carson@valrsoft.com>
2025-03-17 12:36:14 +0100

    fixup eslint

--- playwright/errors.spec.ts
+++ playwright/errors.spec.ts
@@ -1,3 +1,10 @@
+/*
+Copyright 2025 New Vector Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE in the repository root for full details.
+*/
+
 import { expect, test } from "@playwright/test";
 
 test("Should show error screen if fails to get JWT token", async ({ page }) => {
