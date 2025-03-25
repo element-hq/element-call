0fbde40359b3a37e2dcd64fae888130096e1c65d
Valere <bill.carson@valrsoft.com>
2025-03-25 08:18:34 +0100

    test: End to end integrated test for call widget

---
+++ playwright/global.d.ts
@@ -0,0 +1,16 @@
+/*
+Copyright 2025 New Vector Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE in the repository root for full details.
+*/
+
+import type * as Matrix from "matrix-js-sdk/src";
+
+declare global {
+  interface Window {
+    mxMatrixClientPeg: {
+      get(): Matrix.MatrixClient;
+    };
+  }
+}
