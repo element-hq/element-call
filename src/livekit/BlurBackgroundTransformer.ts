0a3fcf036178c3465f54d2d84245211a47582b37
Robin <robin@robin.town>
2025-04-28 15:58:50 +0200

    Import the segmenter model in a more statically-analyzable way

--- src/livekit/BlurBackgroundTransformer.ts
+++ src/livekit/BlurBackgroundTransformer.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 New Vector Ltd.
+Copyright 2024-2025 New Vector Ltd.
 
 SPDX-License-Identifier: AGPL-3.0-only
 Please see LICENSE in the repository root for full details.
@@ -11,6 +11,8 @@ import {
   type VideoTransformerInitOptions,
 } from "@livekit/track-processors";
 import { ImageSegmenter } from "@mediapipe/tasks-vision";
+
+import modelAssetPath from "../mediapipe/imageSegmenter/selfie_segmenter.tflite?url";
 
 interface WasmFileset {
   /** The path to the Wasm loader script. */
@@ -31,11 +33,6 @@ const wasmFileset: WasmFileset = {
     import.meta.url,
   ).href,
 };
-
-const modelAssetPath = new URL(
-  "../mediapipe/imageSegmenter/selfie_segmenter.tflite",
-  import.meta.url,
-).href;
 
 export class BlurBackgroundTransformer extends BackgroundTransformer {
   public async init({
