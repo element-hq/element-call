08ab38280ad382a05b6c8b186663ecfca49f823d
fkwp <github-fkwp@w4ve.de>
2025-05-30 14:49:29 +0200

    prettier

--- playwright/fixtures/widget-user.ts
+++ playwright/fixtures/widget-user.ts
@@ -5,7 +5,13 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE in the repository root for full details.
 */
 
-import { type Browser, type Page, test, expect, type JSHandle } from "@playwright/test";
+import {
+  type Browser,
+  type Page,
+  test,
+  expect,
+  type JSHandle,
+} from "@playwright/test";
 
 import type { MatrixClient } from "matrix-js-sdk";
 
