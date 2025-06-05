e1e41b9de22123bc4ec465161b7fac1f240fe84f
Andrew Ferrazzutti <andrewf@element.io>
2025-06-05 10:18:46 -0400

    Build test image to local Docker registry

--- scripts/playwright-webserver-command.sh
+++ scripts/playwright-webserver-command.sh
@@ -2,7 +2,7 @@
 if [ -n "$USE_DOCKER" ]; then
     set -ex
     yarn build
-    docker build -t element-call:testing .
+    docker build -t localhost:5000/element-call:testing .
     exec docker run --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config/json:ro,Z element-call:testing
 else
     cp config/config.devenv.json public/config.json
