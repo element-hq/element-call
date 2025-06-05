925df134735b9cfb9db20574a2218d983117abd4
Andrew Ferrazzutti <andrewf@element.io>
2025-06-05 14:58:58 -0400

    Upload nginx logs for Playwright CI test

--- scripts/playwright-webserver-command.sh
+++ scripts/playwright-webserver-command.sh
@@ -4,7 +4,9 @@ if [ -n "$USE_DOCKER" ]; then
     yarn build
     IMAGE_NAME=localhost:5000/element-call:testing
     docker build -t "$IMAGE_NAME" .
-    exec docker run --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config/json:ro,Z "$IMAGE_NAME"
+    LOG_DIR=./webserver-logs
+    mkdir -p "$LOG_DIR"
+    exec docker run --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config/json:ro,Z "$IMAGE_NAME" >"${LOG_DIR}/access.log" 2>"${LOG_DIR}/error.log"
 else
     cp config/config.devenv.json public/config.json
     exec yarn dev
