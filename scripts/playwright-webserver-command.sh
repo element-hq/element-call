d8ec2e6e20bed20bfc668f4622cbc5f4b4afa0d8
Andrew Ferrazzutti <andrewf@element.io>
2025-06-06 08:26:56 -0400

    Revert debugging changes for Playwright

--- scripts/playwright-webserver-command.sh
+++ scripts/playwright-webserver-command.sh
@@ -2,11 +2,8 @@
 if [ -n "$USE_DOCKER" ]; then
     set -ex
     yarn build
-    IMAGE_NAME=localhost:5000/element-call:testing
-    docker build -t "$IMAGE_NAME" .
-    LOG_DIR=./webserver-logs
-    mkdir -p "$LOG_DIR"
-    exec docker run --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config.json:ro,Z "$IMAGE_NAME" >"${LOG_DIR}/access.log" 2>"${LOG_DIR}/error.log"
+    docker build -t element-call:testing .
+    exec docker run --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config.json:ro,Z element-call-testing
 else
     cp config/config.devenv.json public/config.json
     exec yarn dev
