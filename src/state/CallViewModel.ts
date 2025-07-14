8458d198c99c5c8190e54f539ddedc03b909348b
Timo <16718859+toger5@users.noreply.github.com>
2025-07-14 12:53:09 +0200

    Fix bluetooth iOS issue (device auto switches from Bluetooth to speaker) (#3388)
    * fix ios bluetooth
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * fix lints
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    ---------
    
    Signed-off-by: Timo K <toger5@hotmail.de>

--- src/state/CallViewModel.ts
+++ src/state/CallViewModel.ts
@@ -1276,7 +1276,7 @@ export class CallViewModel extends ViewModel {
     (available, selected) => {
       const selectionType = selected && available.get(selected.id)?.type;
 
-      // If we are in any output mode other than spaeker switch to speaker.
+      // If we are in any output mode other than speaker switch to speaker.
       const newSelectionType =
         selectionType === "speaker" ? "earpiece" : "speaker";
       const newSelection = [...available].find(
