8c1fa3fd703a2a48a483dc7129e0b76f737734d3
Robin <robin@robin.town>
2025-08-14 14:57:39 +0200

    Fix inaccurate copyright headers

--- i18next-parser.config.ts
+++ i18next-parser.config.ts
@@ -1,9 +1,3 @@
-/*
-Copyright 2024 New Vector Ltd.
-SPDX-License-Identifier: AGPL-3.0-only
-Please see LICENSE in the repository root for full details.
-*/
-
 import type { UserConfig } from "i18next-parser";
 
 const config: UserConfig = {
