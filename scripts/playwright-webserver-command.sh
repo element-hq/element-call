0823936ee1fd091c6356fc0eda3eb0fc83ad628f
Valere <bill.carson@valrsoft.com>
2025-10-23 16:05:24 +0200

    revert CI test changes

--- scripts/playwright-webserver-command.sh
+++ scripts/playwright-webserver-command.sh
@@ -3,7 +3,7 @@ if [ -n "$USE_DOCKER" ]; then
     set -ex
     yarn build
     docker build -t element-call:testing .
-    exec docker run --log-level debug --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config.json:ro,Z element-call:testing
+    exec docker run --rm --name element-call-testing -p 8080:8080 -v ./config/config.devenv.json:/app/config.json:ro,Z element-call:testing
 else
     cp config/config.devenv.json public/config.json
     exec yarn dev
