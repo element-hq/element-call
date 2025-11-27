83b599a4c4013f6e557568da259713bb95bf0e75
Valere <bill.carson@valrsoft.com>
2025-11-27 16:39:45 +0100

    vitest: Add threshold on vitest to check locally

--- vitest.config.ts
+++ vitest.config.ts
@@ -25,6 +25,12 @@ export default defineConfig((configEnv) =>
             "src/utils/test-fixtures.ts",
             "playwright/**",
           ],
+          thresholds: {
+            lines: 80,
+            functions: 80,
+            branches: 80,
+            statements: 80,
+          },
         },
       },
     }),
