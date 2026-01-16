5845db37cc250d72c3fec8a4d904eb89d506b285
Timo K <toger5@hotmail.de>
2026-01-16 10:22:23 +0100

    remove widget layout widget action for good.

--- src/room/InCallView.tsx
+++ src/room/InCallView.tsx
@@ -435,19 +435,6 @@ export const InCallView: FC<InCallViewProps> = ({
     [vm],
   );
 
-  // useEffect(() => {
-  //   widget?.api.transport
-  //     .send(
-  //       gridMode === "grid"
-  //         ? ElementWidgetActions.TileLayout
-  //         : ElementWidgetActions.SpotlightLayout,
-  //       {},
-  //     )
-  //     .catch((e) => {
-  //       logger.error("Failed to send layout change to widget API", e);
-  //     });
-  // }, [gridMode]);
-
   useEffect(() => {
     if (widget) {
       const onTileLayout = (ev: CustomEvent<IWidgetApiRequest>): void => {
