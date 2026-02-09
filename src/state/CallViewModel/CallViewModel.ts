13d131c2e939fee8e6b6eb0317eb8078eb23c022
Robin <robin@robin.town>
2026-02-09 19:07:27 +0100

    Logically separate the advertised transport from the active transport
    To correctly implement the legacy "oldest membership" mode, we need the code to be more nuanced about the local transport. Specifically, it needs to allow for the transport we advertise in our membership to be different from the transport that we connect to and publish media on. Otherwise, if these two are yoked together, members will resend their memberships whenever an SFU hop occurs, which an attacker could use to cause an amplified wave of state changes.

--- src/state/CallViewModel/CallViewModel.ts
+++ src/state/CallViewModel/CallViewModel.ts
@@ -444,35 +444,34 @@ export function createCallViewModel$(
     memberId: uuidv4(),
   };
 
-  const localTransport$ = createLocalTransport$({
-    scope: scope,
-    memberships$: memberships$,
-    ownMembershipIdentity,
-    client,
-    delayId$: scope.behavior(
-      (
-        fromEvent(
-          matrixRTCSession,
-          MembershipManagerEvent.DelayIdChanged,
-          // The type of reemitted event includes the original emitted as the second arg.
-        ) as Observable<[string | undefined, IMembershipManager]>
-      ).pipe(map(([delayId]) => delayId ?? null)),
-      matrixRTCSession.delayId ?? null,
-    ),
-    roomId: matrixRoom.roomId,
-    forceJwtEndpoint$: scope.behavior(
-      matrixRTCMode$.pipe(
-        map((v) =>
-          v === MatrixRTCMode.Matrix_2_0
-            ? JwtEndpointVersion.Matrix_2_0
-            : JwtEndpointVersion.Legacy,
-        ),
+  const localTransport$ = scope.behavior(
+    matrixRTCMode$.pipe(
+      map((mode) =>
+        createLocalTransport$({
+          scope: scope,
+          memberships$: memberships$,
+          ownMembershipIdentity,
+          client,
+          delayId$: scope.behavior(
+            (
+              fromEvent(
+                matrixRTCSession,
+                MembershipManagerEvent.DelayIdChanged,
+                // The type of reemitted event includes the original emitted as the second arg.
+              ) as Observable<[string | undefined, IMembershipManager]>
+            ).pipe(map(([delayId]) => delayId ?? null)),
+            matrixRTCSession.delayId ?? null,
+          ),
+          roomId: matrixRoom.roomId,
+          forceJwtEndpoint:
+            mode === MatrixRTCMode.Matrix_2_0
+              ? JwtEndpointVersion.Matrix_2_0
+              : JwtEndpointVersion.Legacy,
+          useOldestMember: mode === MatrixRTCMode.Legacy,
+        }),
       ),
     ),
-    useOldestMember$: scope.behavior(
-      matrixRTCMode$.pipe(map((v) => v === MatrixRTCMode.Legacy)),
-    ),
-  });
+  );
 
   const connectionFactory = new ECConnectionFactory(
     client,
@@ -491,6 +490,7 @@ export function createCallViewModel$(
     connectionFactory: connectionFactory,
     localTransport$: scope.behavior(
       localTransport$.pipe(
+        switchMap((t) => t.active$),
         catchError((e: unknown) => {
           logger.info(
             "could not pass local transport to createConnectionManager$. localTransport$ threw an error",
@@ -524,13 +524,13 @@ export function createCallViewModel$(
   );
 
   const localMembership = createLocalMembership$({
-    scope: scope,
+    scope,
     homeserverConnected: createHomeserverConnected$(
       scope,
       client,
       matrixRTCSession,
     ),
-    muteStates: muteStates,
+    muteStates,
     joinMatrixRTC: (transport: LivekitTransportConfig) => {
       return enterRTCSession(
         matrixRTCSession,
@@ -550,9 +550,11 @@ export function createCallViewModel$(
         ),
       );
     },
-    connectionManager: connectionManager,
-    matrixRTCSession: matrixRTCSession,
-    localTransport$: localTransport$,
+    connectionManager,
+    matrixRTCSession,
+    localTransport$: scope.behavior(
+      localTransport$.pipe(switchMap((t) => t.advertised$)),
+    ),
     logger: logger.getChild(`[${Date.now()}]`),
   });
 
