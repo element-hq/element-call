364fdb5e543b2ab4666e31598f549925aa665ef5
Robin <robin@robin.town>
2026-04-13 15:43:39 +0200

    Get rid of the 'open in app' mobile prompt
    We've determined that calls are working fine in mobile browsers these days and we no longer need it.

--- src/UrlParams.ts
+++ src/UrlParams.ts
@@ -160,13 +160,6 @@ export interface UrlConfiguration {
    */
   confineToRoom: boolean;
   /**
-   * Whether upon entering a room, the user should be prompted to launch the
-   * native mobile app. (Affects only Android and iOS.)
-   *
-   * The app prompt must also be enabled in the config for this to take effect.
-   */
-  appPrompt: boolean;
-  /**
    * Whether the app should pause before joining the call until it sees an
    * io.element.join widget action, allowing it to be preloaded.
    */
@@ -256,26 +249,6 @@ export interface UrlConfiguration {
 // clearer what each flag means, and helps us avoid coupling Element Call's
 // behavior to the needs of specific consumers.
 export interface UrlParams extends UrlProperties, UrlConfiguration {}
-
-// This is here as a stopgap, but what would be far nicer is a function that
-// takes a UrlParams and returns a query string. That would enable us to
-// consolidate all the data about URL parameters and their meanings to this one
-// file.
-export function editFragmentQuery(
-  hash: string,
-  edit: (params: URLSearchParams) => URLSearchParams,
-): string {
-  const fragmentQueryStart = hash.indexOf("?");
-  const fragmentParams = edit(
-    new URLSearchParams(
-      fragmentQueryStart === -1 ? "" : hash.substring(fragmentQueryStart),
-    ),
-  );
-  return `${hash.substring(
-    0,
-    fragmentQueryStart,
-  )}?${fragmentParams.toString()}`;
-}
 
 class ParamParser {
   private fragmentParams: URLSearchParams;
@@ -392,7 +365,6 @@ export const computeUrlParams = (search = "", hash = ""): UrlParams => {
   // Here we only use constants and `platform` to determine the intent preset.
   let intentPreset: UrlConfiguration = {
     confineToRoom: true,
-    appPrompt: false,
     preload: false,
     header: platform === "desktop" ? HeaderStyle.None : HeaderStyle.AppBar,
     showControls: true,
@@ -448,7 +420,6 @@ export const computeUrlParams = (search = "", hash = ""): UrlParams => {
     default:
       intentPreset = {
         confineToRoom: false,
-        appPrompt: true,
         preload: false,
         header: HeaderStyle.Standard,
         showControls: true,
@@ -493,7 +464,6 @@ export const computeUrlParams = (search = "", hash = ""): UrlParams => {
 
   const configuration: Partial<UrlConfiguration> = {
     confineToRoom: parser.getFlag("confineToRoom"),
-    appPrompt: parser.getFlag("appPrompt"),
     preload: isWidget ? parser.getFlag("preload") : undefined,
     // Check hideHeader for backwards compatibility. If header is set, hideHeader
     // is ignored.
