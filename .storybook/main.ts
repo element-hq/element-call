6dd3b2515e122114b9c9b05d34a1c68fb1e4a03b
Timo K <toger5@hotmail.de>
2026-04-14 14:30:46 +0200

    knip

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-docs", "@storybook/addon-vitest"],
+  addons: ["@storybook/addon-docs"],
   framework: "@storybook/react-vite",
 };
 export default config;
