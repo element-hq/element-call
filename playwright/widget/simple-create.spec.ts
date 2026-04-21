701edd952273de7ba77bd3309cdcb8227eb793f2
Valere <bill.carson@valrsoft.com>
2026-04-21 17:54:10 +0200

    fixup lint

--- playwright/widget/simple-create.spec.ts
+++ playwright/widget/simple-create.spec.ts
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 Please see LICENSE in the repository root for full details.
 */
 
-import { expect, test } from "@playwright/test";
+import { expect } from "@playwright/test";
 
 import { widgetTest } from "../fixtures/widget-user.ts";
 import { TestHelpers } from "./test-helpers.ts";
